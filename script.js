a7c90da8964afa10fac8d7a6e38f07e47271e13a
Roman Masovskiy <130085129+masovskiy@users.noreply.github.com>
2024-10-03 21:46:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-
 const title = document.querySelector('.title');
 title.style.opacity = 0; 
 window.addEventListener('load', () => {
-  title.style.transition = 'opacity 1s ease-in-out'; 
-  title.style.opacity = 1; 
+ title.style.transition = 'opacity 1s ease-in-out'; 
+ title.style.opacity = 1; 
 });
 
-
+// Изменение цвета ссылок при наведении
 const navLinks = document.querySelectorAll('nav a');
 navLinks.forEach(link => {
-  link.addEventListener('mouseover', () => {
-    link.style.color = '#ff8c00'; 
-  });
-  link.addEventListener('mouseout', () => {
-    link.style.color = '#333'; 
-  });
+ link.addEventListener('mouseover', () => {
+  link.style.color = '#ff8c00'; 
+ });
+ link.addEventListener('mouseout', () => {
+  link.style.color = '#333';
+ });
 });
